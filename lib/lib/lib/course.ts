2cc4b3b1c236db345f175e6d99d559ec82f065a7
thiagofrauche <thiagofrauche@gmail.com>
2025-11-22 13:03:22 -0300

    Delete lib/lib/lib/course.ts

--- lib/lib/lib/course.ts
+++
@@ -1,8 +0,0 @@
-// Carrega o dataset local do curso
-import dataset from '@/data/course_dataset.json';
-
-export type Course = typeof dataset.course;
-
-export function getCourse() {
-  return dataset.course;
-}
